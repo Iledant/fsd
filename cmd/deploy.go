f4e9095c71e8ee1d80f8b4a1db12563fc04d1270
Iledant <christophe.saintillan@gmail.com>
2019-12-26 11:22:35 +0100

    First functionnal version

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -1,20 +1,43 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v2"
 )
 
 var (
-	deployCmd = &cobra.Command{
+	rootCmd = &cobra.Command{
 		Use:   "fsd",
-		Short: "fsd compile le back- et le frontend et déploie aur AWS",
-		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Println("Appel de la commande")
+		Short: "Utilitaire pour le déploiement d'applications sur AWS",
+	}
+	deployCmd = &cobra.Command{
+		Use:   "deploy",
+		Short: "Compile le backend et le frontend et le déploie sur AWS",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return errors.New("deploy ne prend qu'un argument : le nom de l'application")
+			}
+			for _, app := range cfg.Application {
+				if args[0] == app.Name {
+					return nil
+				}
+			}
+			return errors.New("application non trouvée dans le fichier de configuration")
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			for _, a := range cfg.Application {
+				if a.Name == args[0] {
+					return launch(a)
+				}
+			}
+			return fmt.Errorf("impossible de trouver l'application %s", args[0])
 		},
 	}
 	cfgFile string
@@ -26,27 +49,47 @@ type config struct {
 }
 
 type fullStackCfg struct {
-	Name     string  `yaml:"name"`
-	BackEnd  partCfg `yaml:"backend"`
-	FrontEnd partCfg `yaml:"frontend"`
-	Deploy   partCfg `yaml:"deploy"`
+	Name     string    `yaml:"name"`
+	BackEnd  partCfg   `yaml:"backend"`
+	FrontEnd partCfg   `yaml:"frontend"`
+	Deploy   deployCfg `yaml:"deploy"`
 }
 
 type partCfg struct {
-	Path    string `yaml:"path"`
-	Command string `yaml:"command"`
+	Path        string   `yaml:"path"`
+	Command     string   `yaml:"command"`
+	Args        []string `yaml:"args"`
+	Environment []envVar `yaml:"environment"`
+}
+
+type deployCfg struct {
+	Path      string   `yaml:"path"`
+	Command   string   `yaml:"command"`
+	Args      []string `yaml:"args"`
+	Dist      dist     `yaml:"dist"`
+	AppSource string   `yaml:"app_source"`
+}
+
+type dist struct {
+	Source string `yaml:"source"`
+	Dest   string `yaml:"dest"`
+}
+
+type envVar struct {
+	Name  string `yaml:"name"`
+	Value string `yaml:"value"`
 }
 
 // Execute launches the deploy command
 func Execute() error {
-	return deployCmd.Execute()
+	return rootCmd.Execute()
 }
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	deployCmd.PersistentFlags().StringVar(&cfgFile, "config", "",
-		"fichier de configuration, par défaut $HOME/.fsd.yaml")
+	rootCmd.AddCommand(deployCmd)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "",
+		"fichier de configuration, par défaut ~/.fsd.yaml")
 }
 
 func initConfig() {
@@ -63,8 +106,105 @@ func initConfig() {
 		fmt.Printf("impossible de lire le fichier de configuration %v", err)
 		os.Exit(1)
 	}
-	if err = yaml.Unmarshal(content,&cfg) ; err !=nil {
+	if err = yaml.Unmarshal(content, &cfg); err != nil {
 		fmt.Printf("erreur de décodage du fichier de configuration %v", err)
 		os.Exit(1)
 	}
+}
+
+func launch(c fullStackCfg) error {
+	// fmt.Println("Compilation du backend")
+	// if err := launchPart(c.BackEnd); err != nil {
+	// 	return err
+	// }
+	fmt.Println("Compilation du frontend")
+	if err := launchPart(c.FrontEnd); err != nil {
+		return err
+	}
+	fmt.Println("Déploiement")
+	return launchDeploy(c.Deploy)
+}
+
+func launchPart(p partCfg) error {
+	if err := os.Chdir(p.Path); err != nil {
+		return fmt.Errorf("changement de répertoire %s : %v", p.Path, err)
+	}
+	cmd := exec.Command(p.Command)
+	for _, a := range p.Args {
+		cmd.Args = append(cmd.Args, a)
+	}
+	for _, e := range p.Environment {
+		os.Setenv(e.Name, e.Value)
+	}
+	out, err := cmd.Output()
+	if err != nil {
+		return fmt.Errorf("erreur d'exécution : %v", err)
+	}
+	fmt.Printf("%s", out)
+	return nil
+}
+
+func launchDeploy(d deployCfg) error {
+	if err := os.Chdir(d.Path); err != nil {
+		return fmt.Errorf("changement de répertoire %s : %v", d.Path, err)
+	}
+	dest := d.Path + "\\" + d.Dist.Dest
+	if err := os.RemoveAll(dest); err != nil {
+		return fmt.Errorf("suppression du répertoire dist : %v", err)
+	}
+	if err := os.Mkdir(dest, os.ModeDir|os.ModePerm); err != nil {
+		return fmt.Errorf("changement de répertoire %s : %v", d.Path, err)
+	}
+	if err := copyFilesAndDirs(d.Dist.Source, dest); err != nil {
+		return err
+	}
+	cmd := exec.Command(d.Command)
+	for _, a := range d.Args {
+		cmd.Args = append(cmd.Args, a)
+	}
+	out, err := cmd.Output()
+	if err != nil {
+		return fmt.Errorf("erreur d'exécution : %v", err)
+	}
+	fmt.Printf("%s", out)
+	return nil
+}
+
+func copyFilesAndDirs(src, dest string) error {
+	var dirs []string
+	files, err := ioutil.ReadDir(src)
+	if err != nil {
+		return fmt.Errorf("impossible de lire le contenu du répertoire %s : %v", src, err)
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			if err = os.Mkdir(dest+"\\"+file.Name(), os.ModeDir|os.ModePerm); err != nil {
+				return fmt.Errorf("impossible de créer le répertoire %s : %v", file.Name(), err)
+			}
+			dirs = append(dirs, file.Name())
+			continue
+		}
+		if file.Mode().IsRegular() {
+			src, err := os.Open(src + "\\" + file.Name())
+			if err != nil {
+				return fmt.Errorf("impossible d'ouvrir le fichier %s : %v", file.Name(), err)
+			}
+			defer src.Close()
+			dst, err := os.Create(dest + "\\" + file.Name())
+			if err != nil {
+				return fmt.Errorf("impossible d'ouvrir le fichier %s : %v", file.Name(), err)
+			}
+			defer dst.Close()
+			_, err = io.Copy(dst, src)
+			if err != nil {
+				return fmt.Errorf("impossible de copier le fichier %s : %v", file.Name(), err)
+			}
+		}
+	}
+	for _, d := range dirs {
+		if err = copyFilesAndDirs(src+"\\"+d, dest+"\\"+d); err != nil {
+			return err
+		}
+	}
+	return nil
 }
